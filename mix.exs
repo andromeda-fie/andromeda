e712061318baf5ce3dbb6eca62722b11d38a8d7f
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-03-31 20:02:43 -0300

    feat: add CI

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Andromeda.MixProject do
       {:jason, "~> 1.2"},
       {:bandit, "~> 1.2"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
